b69ba653ca234f5b8421a9f1b5de15df7c1f0b66
Dan Cuomo <dcuomo@outlook.com>
2021-03-21 21:44:05 -0700

    adding reloader

---
+++ reload.ps1
@@ -0,0 +1,18 @@
+cd C:\Test-NetStack
+
+Remove-Module Test-NetStack -Force -ErrorAction SilentlyContinue
+Remove-Module icmp          -Force -ErrorAction SilentlyContinue
+Remove-Module prerequisites -Force -ErrorAction SilentlyContinue
+Remove-Module internal      -Force -ErrorAction SilentlyContinue
+Remove-Module icmp          -Force -ErrorAction SilentlyContinue
+Remove-Module udp           -Force -ErrorAction SilentlyContinue
+Remove-Module tcp           -Force -ErrorAction SilentlyContinue
+Remove-Module ndk           -Force -ErrorAction SilentlyContinue
+
+psedit .\Test-NetStack.psm1
+#psedit .\helpers\icmp.psm1
+
+ipmo .\Test-NetStack.psd1 -Force
+$s = Test-NetStack -Nodes HV01, HV02
+
+#$s = Test-NetStack -IPTarget 172.16.0.11, 172.16.0.12
